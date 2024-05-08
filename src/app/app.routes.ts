90fb773a14de95380dd2ac5773e3dc3ef3b543fd
toiluoicode <104342280+toiluoicode@users.noreply.github.com>
2024-05-08 15:47:36 +0700

    danh muc ket noi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { RegisterComponent } from './register/register.component';
 import { ContentComponent } from './content/content.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SamplepageComponent } from './samplepage/samplepage.component';
+import { ConnectionComponent } from './connection/connection.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/samplepage', pathMatch: 'full' },  // Điều hướng trang mặc định tới /login
+    { path: '', redirectTo: '/connection', pathMatch: 'full' },  // Điều hướng trang mặc định tới /login
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'dashboard', component: DashboardComponent},  // Bảo vệ trang Dashboard
     { path: 'content', component: ContentComponent },  // Bảo vệ trang Content
-    { path: 'samplepage', component: SamplepageComponent }
+    { path: 'samplepage', component: SamplepageComponent },
+    {path:'connection',component:ConnectionComponent}
 ];
