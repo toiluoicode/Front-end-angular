963820f5f3f9ab0282da7bb0ea0e24fa5968a4b1
“Hoanganhz12” <hoanganhv075@gmail.com>
2024-05-08 13:53:01 +0700

    fe3

--- src/app/samplepage/samplepage.component.ts
+++ src/app/samplepage/samplepage.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './../header/header.component';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
@@ -5,10 +6,18 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-samplepage',
   standalone: true,
-  imports: [RouterLink,RouterOutlet,HeaderComponent],
+  imports: [RouterLink,RouterOutlet,HeaderComponent,CommonModule],
   templateUrl: './samplepage.component.html',
   styleUrl: './samplepage.component.css'
 })
 export class SamplepageComponent {
+  submenuVisible1: boolean = false;
+  submenuVisible2: boolean = false;
 
+  toggleSubMenu1() {
+    this.submenuVisible1 = !this.submenuVisible1;
+  }
+  toggleSubMenu2() {
+    this.submenuVisible2 = !this.submenuVisible2;
+  }
 }
